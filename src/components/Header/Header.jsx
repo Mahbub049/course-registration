e6618bcf281005a224041d488a0ed2f4c7d9716b
Muhammad Mahbub Sarwar Shafi <mahbubsarwar49@gmail.com>
2024-03-14 02:35:00 +0600

    project setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="container mx-auto mt-[50px] mb-[32px]">
+            <h1 className="text-[32px] font-bold text-center">Course Registration</h1>
+        </div>
+    );
+};
+
+export default Header;
